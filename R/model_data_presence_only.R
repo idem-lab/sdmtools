82310b2036e750a85f9cd37b00e98d945d9f4285
geryan <geryan@users.noreply.github.com>
2023-06-09 15:30:55 +1000

    model_data_presence_only

---
+++ R/model_data_presence_only.R
@@ -0,0 +1,41 @@
+#' Title
+#'
+#' @param presences
+#' @param absences
+#' @param covariates
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+model_data_presence_only <- function(
+    presences,
+    absences,
+    covariates
+){
+
+  pvals <- terra::extract(
+    covariates,
+    presences %>%
+      dplyr::select(x, y)
+  )
+  avals <- terra::extract(
+    covariates,
+    absences %>%
+      dplyr::select(x, y)
+  )
+
+  rbind(
+    pvals %>%
+      tibble::as_tibble %>%
+      dplyr::select(-ID) %>%
+      mutate(presence = 1),
+    avals %>%
+      tibble::as_tibble %>%
+      dplyr::select(-ID) %>%
+      mutate(presence = 0)
+  ) %>%
+    tibble::as_tibble
+
+}
