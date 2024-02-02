a5dd6e4276addcb3ec7f2e2a9b26830eb4b8b591
geryan <geryan@users.noreply.github.com>
2024-02-02 15:07:50 +1100

    fix doc `raster_to_terra`

--- R/raster_to_terra.R
+++ R/raster_to_terra.R
@@ -4,14 +4,14 @@
 #' @name raster_to_terra
 #'
 #' @format ## `global_regions`
-#' A tibble with 194 rows and 5 columns:
+#' A tibble with 42 rows and 3 columns:
 #' \describe{
 #'   \item{raster}{function name in `raster`}
 #'   \item{terra}{function name in `terra`}
 #'   \item{comment}{Annotations about differences in arguments or usage, etc.}
 #' }
 #'
-#' @source Initially produced by Gerry Ryan and supplemented with "New method names" section in https://cran.r-project.org/web/packages/terra/terra.pdf
+#' @source Initially produced by Gerry Ryan and supplemented with "New method names" section in https://cran.r-project.org/web/packages/terra/terra.pdf, annotations added by IDEM members
 #' @details
 #' Some argument names may differ.
 #'
