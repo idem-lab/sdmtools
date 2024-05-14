c124c27f3c9c4237f109a6d3a29d367ab1e0027b
geryan <geryan@users.noreply.github.com>
2024-05-14 11:57:10 +1000

    Drank the slab that Bon Scott drunk

--- tests/testthat/test-mask_all.R
+++ tests/testthat/test-mask_all.R
@@ -8,7 +8,7 @@ testthat::test_that("is a SpatRaster", {
    sdmtools::example_raster(seed = 2),
    sdmtools::example_raster(seed = 3)
   )
-  rvals <- terra::values(rasts)
+  rvals <- terra::values(r)
   nas <- c(1:10, 105:120, 215:240)
   rvals[nas] <- NA
   r[] <- rvals
@@ -17,4 +17,15 @@ testthat::test_that("is a SpatRaster", {
   testthat::expect_s4_class(s, "SpatRaster")
 
   testthat::expect_equal(dim(r), dim(s))
+
+  svals <- terra::values(s)
+
+  z <- apply(svals, c(1, 2), is.na)
+
+  z1 <- z[,1]
+
+  x <- apply(z, 2, FUN = function(x, y){ifelse(y, x, !x)}, y = z1)
+
+  testthat::expect_true(all(x))
+
 })
