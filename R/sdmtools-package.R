e440f9f68b7682a860ffe9689544a0b329c05275
njtierney <nicholas.tierney@gmail.com>
2024-05-15 12:45:14 +1000

    use_package_doc() and place globalVariables() inside this to avoid CRAN NOTE

---
+++ R/sdmtools-package.R
@@ -0,0 +1,39 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
+globalVariables(c(
+  "ID ",
+  "as_tibble",
+  "continent",
+  "dev.off",
+  "extract",
+  "geometry",
+  "iso3",
+  "ID",
+  "mask",
+  "mutate",
+  "png",
+  "predict",
+  "quantile",
+  "r",
+  "rlnorm",
+  "runif",
+  "select",
+  "seq_length",
+  "setNames",
+  "st_as_sf",
+  "st_coordinates",
+  "values",
+  "x",
+  "xmax",
+  "xmin",
+  "xyFromCell",
+  "y",
+  "ymax",
+  "ymin"
+))
