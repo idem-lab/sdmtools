8ede628e08fbc99a92264e25ff3c91f750930016
geryan <geryan@users.noreply.github.com>
2024-06-20 12:53:38 +1000

    One maintains standards or one has none.

--- R/std_rast.R
+++ R/std_rast.R
@@ -1,12 +1,12 @@
 #' @title Standardise raster
 #' @description Standardises all layers in a `SpatRaster` to a scale of 0-1,
-#' by dividing by the maximum value in each layer.
+#' by dividing by the maximum value in each layer. *Only operates by layer*
 #'
 #' @param x `SpatRaster` to standardise
 #' @param filename Optional `character` path and filename to write output
 #' @param overwrite `logical` if `TRUE` will overwrite `filename`
 #'
-#' @return
+#' @return A `SpatRaster` with range 0-1
 #' @export
 #'
 #' @details
@@ -17,26 +17,20 @@
 #' example_raster() |>
 #'   std_rast()
 std_rast <- function(x, filename = NULL, overwrite = TRUE){
-  if(terra::nlyr(x)>1){
+  vals <- terra::values(x)
 
-    vals <- terra::values(x)
+  nvs <- apply(
+    vals,
+    MARGIN = 2,
+    FUN = function(x){
+      x/max(x, na.rm = TRUE)
+    }
+  )
 
-    nvs <- apply(
-      vals,
-      MARGIN = 2,
-      FUN = function(x){
-        x/max(x, na.rm = TRUE)
-      }
-    )
+  r <- x
 
-    r <- x
+  r[] <- nvs
 
-    r[] <- nvs
-
-
-  } else {
-    r <- x / max(x)
-  }
 
   if(is.null(filename)){
     return(r)
