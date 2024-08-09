a42a196f244a6cfec5a26635fc8daabdc0e0ab30
geryan <geryan@users.noreply.github.com>
2024-08-09 16:39:30 +1000

    To dress this up so bloodless

--- R/std_rast.R
+++ R/std_rast.R
@@ -3,6 +3,7 @@
 #' by dividing by the maximum value in each layer. *Only operates by layer*
 #'
 #' @param x `SpatRaster` to standardise
+#' @param reverse `logical` if `TRUE` will subtract standardised values from 1
 #' @param filename Optional `character` path and filename to write output
 #' @param overwrite `logical` if `TRUE` will overwrite `filename`
 #'
@@ -14,9 +15,9 @@
 #'
 #'
 #' @examples
-#' example_raster() |>
-#'   std_rast()
-std_rast <- function(x, filename = NULL, overwrite = TRUE){
+#' example_raster(seed = 3010) |>
+#'   std_rast(reverse = TRUE)
+std_rast <- function(x, reverse, filename = NULL, overwrite = TRUE){
   vals <- terra::values(x)
 
   nvs <- apply(
@@ -28,6 +29,10 @@ std_rast <- function(x, filename = NULL, overwrite = TRUE){
   )
 
   r <- x
+
+  if(reverse){
+    nvs <- 1 - nvs
+  }
 
   r[] <- nvs
 
