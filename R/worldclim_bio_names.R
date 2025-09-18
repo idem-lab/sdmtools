40bab610df8f4295c5b981c97007e85069ee5f23
geryan <geryan@users.noreply.github.com>
2025-09-18 13:26:28 +0300

    add `wordlclim_bio_names`

---
+++ R/worldclim_bio_names.R
@@ -0,0 +1,16 @@
+#' @title WorldClim `"bio"` names
+#' @description Character vector of names for the `geodata::worldclim_global`
+#' etc. `SpatRaster` layers, as a convenience alternative to the default
+#' detailed and accurate but horridly unreadable format of `wc2.1_30s_bio_1`
+#' etc.
+#'
+#' @name worldclim_bio_names
+#'
+#' @format ## `worldclim` `character` vector of names
+#'
+#' @return `character`
+#'
+#' @source \url{https://www.worldclim.org/}
+#'
+
+"worldclim_bio_names"
